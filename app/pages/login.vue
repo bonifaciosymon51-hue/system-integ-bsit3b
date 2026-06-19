b2ced5f4f9175ba889aa3b338b38bde6f61604d4
valenzuelayurie-sys <valenzuelayurie@gmail.com>
2026-06-19 15:06:31 +0800

    update login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,104 +1,76 @@
 <template>
- 
   <v-container class="fill-height d-flex align-center justify-center">
-    
-   
     <v-card width="400" class="pa-6" elevation="2">
-      
-     
-      <v-card-title class="text-center text-h5 font-weight-bold">LOGIN</v-card-title>
-      <v-card-subtitle class="text-center mb-4">Welcome back, please login</v-card-subtitle>
+      <v-card-title class="text-center text-h5 font-weight-bold"
+        >LOGIN</v-card-title
+      >
+      <v-card-subtitle class="text-center mb-4"
+        >Welcome back, please login</v-card-subtitle
+      >
 
-     
       <v-card-text>
-        
-        
-        <v-text-field 
-          label="Email" 
-          prepend-inner-icon="mdi-email" 
+        <v-text-field
+          label="Email"
+          prepend-inner-icon="mdi-email"
           variant="outlined"
           class="mb-2"
         ></v-text-field>
 
-        
-        <v-text-field 
-          label="Password" 
-          type="password" 
-          prepend-inner-icon="mdi-lock" 
+        <v-text-field
+          label="Password"
+          type="password"
+          prepend-inner-icon="mdi-lock"
           variant="outlined"
           class="mb-4"
         ></v-text-field>
 
-       
-        <v-btn color="blue" block size="large" class="mb-3">
-          Sign In
-        </v-btn>
+        <v-btn color="blue" block size="large" class="mb-3"> Sign In </v-btn>
 
         <div class="text-center my-2 text-caption text-grey">OR</div>
 
-        
-        <v-btn color="red-darken-1" block size="large" prepend-icon="mdi-google" @click="handleGooglelogin">
+        <v-btn
+          color="red-darken-1"
+          block
+          size="large"
+          prepend-icon="mdi-google"
+          @click="loginWithGoogle"
+        >
           Sign in with Google
         </v-btn>
-
-        <div ref="googleButton"></div>
-
       </v-card-text>
-
     </v-card>
-
   </v-container>
 </template>
 <script lang="ts" setup>
 //@ts-nocheck
-const config = useRuntimeConfig()
-const googleButton = ref<HTMLDivElement | null>(null);
+const config = useRuntimeConfig();
 
-  declare global {
-    interface Window {
-      google: any;
-    }
-  } 
-const handleGoogleLogin = (response: any) => {
-  const user = parseJwt(response.credential);
-  
-  localStorage.setItem("google_user", JSON.stringify(user));
-  localStorage.setItem("google_token", response.credential);
 
-  navigateTo("/");
-
-  
+declare global {
+  interface Window {
+    google: any;
+  }
 }
+const loginWithGoogle = () => {
+  const client = window.google.accounts.oauth2.initTokenClient({
+    client_id: config.public.googleClientId,
+    scope: 'openid email profile',
+    callback: async (response: any) => {
+      const userInfo = await $fetch(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${response.access_token}`,
+          },
+        }
+      );
+      localStorage.setItem("google_user", JSON.stringify(userInfo));
+      localStorage.setItem("google_token", response.access_token);
 
-const parseJwt = (token: string) => {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  return JSON.parse(
-    decodeURIComponent(
-      atob(base64)
-      .split("")
-      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-      .join("")
-    )
-  )
-}
+      navigateTo("/");
+    },
+  });
 
-onMounted(() => {
-  const timer = setInterval(() => {
-    if (window.google && googleButton.value) {
-      clearInterval(timer)
-
-      window.google.accounts.id.initialize({
-        client_id: config.public.googleClientId,
-        callback: handleGoogleLogin
-      })
-
-      window.google.accounts.id.renderButton(googleButton.value, {
-        theme: 'outline',
-        size: 'large',
-        width: 330
-      })
-    }
-  }, 300)
-})
+  client.requestAccessToken();
+};
 </script>
